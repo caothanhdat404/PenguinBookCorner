1cd6eeb7e10e43a4148ebfa8a2aec179e242c641
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-07-31 23:28:29 +0700

    Display suitable layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 
 function App() {
   return (
@@ -8,15 +9,24 @@ function App() {
       <Router>
         <Routes>
           {routes.map((route) => {
-            const Page = route.page
+            const Page = route.page;
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment;
             return (
-              <Route path={route.path} element={<Page/>} />
-            )
+              <Route
+                key = {route.path}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
