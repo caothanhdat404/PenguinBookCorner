b3a26a54599fb5bceeb45c42bca0bf380ac40b62
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-07-31 20:37:52 +0700

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./routes";
+
 function App() {
   return (
-    <div></div>
-  );
+    <div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            return (
+              <Route path={route.path} element={<Page/>} />
+            )
+          })}
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import HomePage from "../pages/HomePage/HomePage";
+
+export const routes = [
+    {
+        path: '/',
+        page: HomePage
+    }
+]
