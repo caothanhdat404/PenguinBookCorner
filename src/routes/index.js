6215d076ded1f62a2ec5b0c525d8c3c05c5768da
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-07-31 23:26:21 +0700

    Add isShowHeader variable to check if page has header or not

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 import HomePage from "../pages/HomePage/HomePage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 export const routes = [
     {
         path: '/',
-        page: HomePage
+        page: HomePage,
+        isShowHeader: true
+    },
+    {
+        path: '*',
+        page: NotFoundPage,
+        isShowHeader: false
     }
 ]
