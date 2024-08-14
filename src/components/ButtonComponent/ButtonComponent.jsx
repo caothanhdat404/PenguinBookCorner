dd668611ba2eecb943fe1191dc5e1bebf01c26af
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-14 23:55:33 +0700

    Add Button Component

---
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const ButtonComponent = ({ size, styleButton, styleTextButton, textButton, ...rests }) => {
+    return (
+        <Button size={size} style={styleButton} {...rests}>
+            <span style={styleTextButton}>{textButton}</span>
+        </Button>
+    )
+}
+
+export default ButtonComponent
