83b4c085f66031e34edcea2b79d48ea3d81d1f05
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-15 22:51:36 +0700

    Add new routes: ProductDetailsPage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,13 @@ import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import SignInPage from "../pages/SignInPage/SignInPage";
+import ProductDetailsPage from "../pages/ProductDetailsPage/ProductDetailsPage";
 
 export const routes = [
     {
         path: '/',
         page: HomePage,
         isShowHeader: true
-    },
-    {
-        path: '*',
-        page: NotFoundPage,
-        isShowHeader: false
     },
     {
         path: '/sign-up',
@@ -23,5 +19,15 @@ export const routes = [
         path: '/sign-in',
         page: SignInPage,
         isShowHeader: false
-    }
+    },
+    {
+        path: '/product-detail',
+        page: ProductDetailsPage,
+        isShowHeader: true
+    },
+    {
+        path: '*',
+        page: NotFoundPage,
+        isShowHeader: false
+    },
 ]
