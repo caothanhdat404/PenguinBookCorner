1dc6b49a8f9dfa89b1261bb7b0d7b9118ff962c1
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-15 16:41:58 +0700

    Construct SignInPage

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import { WrapperHeaderAuth, HeaderAuth, HeaderLogo, Logo, NeedHelp, WrapperBodyAuth, BodyAuth, Help, OtherWay, SignUpByOtherWay, ChangeState } from './style'
+import { FacebookFilled, GoogleCircleFilled } from '@ant-design/icons'
+
+import FooterComponent from '../../components/FooterComponent/FooterComponent'
+import InputForm from '../../components/InputForm/InputForm'
+import ButtonComponent from '../../components/ButtonComponent/ButtonComponent'
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
+    <div>
+      <WrapperHeaderAuth>
+        <HeaderAuth>
+          <HeaderLogo>
+            <Logo>PEIGUIN BOOK CORNER</Logo>
+            <div style={{ fontSize: '24px' }}>Đăng nhập</div>
+          </HeaderLogo>
+          <NeedHelp>Bạn cần giúp đỡ?</NeedHelp>
+        </HeaderAuth>
+      </WrapperHeaderAuth>
+      <WrapperBodyAuth>
+        <BodyAuth>
+          <div style={{ fontSize: '20px', padding: '22px 0px' }}>Đăng nhập</div>
+          <InputForm style={{marginBottom: '10px', height: '40px'}} placeholder="abc@gmail.com"/>
+          <InputForm style={{marginBottom: '10px', height: '40px'}} placeholder="Password" type="password"/>
+          <ButtonComponent
+            bordered={false}
+            size={40}
+            styleButton={{
+              background: 'rgb(26, 148, 255)',
+              height: '40px',
+              width: '100%',
+              marginTop: '30px',
+              border: 'none',
+              borderRadius: '4px',
+              textAlign: 'center'
+            }}
+            textButton={'Đăng nhập'}
+            styleTextButton={{
+              color: '#fff',
+              fontSize: '16px',
+              fontWeight: '700',
+            }}
+          ></ButtonComponent>
+          <Help>
+            <div>Quên mật khẩu</div>
+            <div>Đăng nhập với SMS</div>
+          </Help>
+          <OtherWay>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', fontSize: '12px', color: 'rgba(0,0,0,.26)' }}>
+              <div style={{ width: '170px', height: '1px', backgroundColor: 'rgba(0,0,0,.26)' }}></div>
+              <div>hoặc</div>
+              <div style={{ width: '170px', height: '1px', backgroundColor: 'rgba(0,0,0,.26)' }}></div>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '30px' }}>
+              <SignUpByOtherWay>
+                <FacebookFilled style={{ color: 'blue', marginRight: '8px' }} />
+                <div>Facebook</div>
+              </SignUpByOtherWay>
+              <SignUpByOtherWay>
+                <GoogleCircleFilled style={{ color: 'red', marginRight: '8px' }} />
+                <div>Google</div>
+              </SignUpByOtherWay>
+            </div>
+          </OtherWay>
+          <ChangeState>Bạn chưa có tài khoản? <a href='/'>Tạo tài khoản</a></ChangeState>
+        </BodyAuth>
+      </WrapperBodyAuth>
+      <FooterComponent />
+    </div>
   )
 }
 
---
+++ src/pages/SignInPage/style.js
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+
+
+export const WrapperHeaderAuth = styled.div`
+    width: 100%;
+    height: 84px;
+    background: white;
+    display: flex;
+    align-items: center;
+    border-width: 0 0 1px 0;
+    border-style: solid;
+    border-color: rgb(235, 235, 240);
+`
+
+export const HeaderAuth = styled.div`
+    width: 100%;
+    height: 32px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 160px
+`
+
+export const HeaderLogo = styled.div`
+    display: flex;
+    align-items: center
+`
+
+export const Logo = styled.div`
+    font-size: 20px;
+    margin-right: 8px;
+    background-color: rgb(26, 148, 255);
+    color: white;
+    padding: 8px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+export const NeedHelp = styled.div`
+    font-size: 14px;
+    color: rgb(26, 148, 255);
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.8
+    }
+`
+
+export const WrapperBodyAuth = styled.div`
+    width: 100%;
+    height: 600px;
+    background-color: rgb(26, 148, 255);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const BodyAuth = styled.div`
+    width: 416px;
+    height: 500px;
+    background-color: white;
+    border-radius: 4px;
+    padding: 0 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export const Help = styled.div`
+    width: 100%;
+    height: 14px;
+    color: rgb(26, 148, 255);
+    font-size: 12px;
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.85;
+    }
+`
+
+export const OtherWay = styled.div`
+    width: 100%;
+    height: 78px;
+`
+
+export const SignUpByOtherWay = styled.div`
+    font-size: 14px;
+    width: 100%;
+    height: 38px;
+    margin: 5px;
+    color: rgba(0,0,0,.87);
+    padding: 0 8px 0 2px;
+    border: 1px solid rgba(0,0,0,.26);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 2px;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.85;
+    }
+`
+
+export const ChangeState = styled.div`
+    font-size: 14px;
+    color: rgba(0, 0, 0, .26);
+    text-align: center;
+    padding: 35px 0 22px 0;
+
+    a {
+        color: rgb(26, 148, 255);
+        text-decoration: none;
+    }
+`
