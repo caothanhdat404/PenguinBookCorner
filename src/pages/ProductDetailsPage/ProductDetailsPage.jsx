5b622f6252f18ff40dd26a3507b2b02003321a05
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-15 22:51:18 +0700

    Create Product Detail Page

---
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductDetailsComponent from '../../components/ProductDetailsComponent/ProductDetailsComponent'
+
+const ProductDetailsPage = () => {
+  return (
+    <div style={{ padding: '0 120px', backgroundColor: '#efefef', height: '1000px' }}>
+      <div>Trang chủ</div>
+      <ProductDetailsComponent />
+    </div>
+  )
+}
+
+export default ProductDetailsPage
