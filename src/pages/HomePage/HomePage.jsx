7586bf119731e7fa655abf25def40193de27d1ee
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-16 19:05:58 +0700

    Build container of HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import SliderComponent from '../../components/SliderComponent/SliderComponent'
+import { WrapperHomePage, WrapperSlider } from './style'
+import slider1 from '../../assets/images/slider1.webp'
+import slider2 from '../../assets/images/slider2.webp'
+import slider3 from '../../assets/images/slider3.webp'
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <WrapperHomePage>
+            <WrapperSlider>
+                <SliderComponent arrImages={[slider1, slider2, slider3]} />
+            </WrapperSlider>
+        </WrapperHomePage>
     );
 }
 
---
+++ src/pages/HomePage/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const WrapperHomePage = styled.div`
+    padding: 0 120px;
+    background-color: #efefef
+`
+
+export const WrapperSlider = styled.div`
+    width: 100%;
+    height: 300px;
+    padding: 16px;
+    background-color: #fff;
+    border-radius: 4px;
+`
