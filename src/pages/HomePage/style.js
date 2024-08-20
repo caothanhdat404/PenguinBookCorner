11fd51cbd79fd6a1025788312f137e1d97db57e6
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-20 11:34:38 +0700

    Fix padding for WrapperHomePage, fix margin for Others, Add Style for WrapperFooter & Wrapper Navbar

--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -2,15 +2,16 @@ import styled from 'styled-components'
 import { Row } from 'antd'
 
 export const WrapperHomePage = styled(Row)`
-    padding: 10px 64px 0;
+    padding: 116px 64px 0;
     background-color: #efefef;
-    height: 1000px;
+    height: 1200px;
 `
 
 export const WrapperSlider = styled.div`
     width: 100%;
     height: 362px;
     padding: 16px;
+    margin-bottom: 10px;
     background-color: #fff;
     border-radius: 4px;
     box-sizing: border-box;
@@ -22,22 +23,61 @@ export const WrapperCardProduct = styled.div`
     align-items: center;
     gap: 10px; 
     background-color: #fff; 
-    margin-top: 20px;
+    margin-bottom: 10px;
     padding: 20px;
     border-radius: 4px; 
     box-sizing: border-box;
 `
 
+export const WrapperFooter = styled.div`
+    width: 100%;
+    height: 345px;
+    padding: 16px;
+    margin-bottom: 10px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-sizing: border-box;
+
+     > div {
+        border: none;
+        padding-right: 40px;
+        padding-left: 40px;
+    }
+`
+
 export const WrapperNavbar = styled.div`
     width: 95%;
-    height: 600px;
+    height: 450px;
     background-color: white;
     border-radius: 4px;
     box-sizing: border-box;
     padding: 10px;
-    overflow-y: scroll;
-    scrollbar-width: none;
-    position: sticky;
-    left: 0;
-    top: 0;
+
+    div {
+        margin-bottom: 8px;
+        padding-left: 16px;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 150%;
+        color: rgb(39, 39, 42);
+    }
+`
+
+export const WrapperSubNavbar = styled.div`
+    width: 95%;
+    height: 140px;
+    background-color: white;
+    border-radius: 4px;
+    box-sizing: border-box;
+    padding: 10px;
+    margin-top: 10px;
+
+    div {
+        margin-bottom: 8px;
+        padding-left: 16px;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 150%;
+        color: rgb(39, 39, 42);
+    }
 `
