dea7d0dbcc162ce8a709b106f8e9cb74e29422f4
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-20 11:26:20 +0700

    Remove Footer Component from Default Component

--- src/components/DefaultComponent/DefaultComponent.jsx
+++ src/components/DefaultComponent/DefaultComponent.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import HeaderComponent from "../HeaderComponent/HeaderComponent";
-import FooterComponent from "../FooterComponent/FooterComponent";
 
 const DefaultComponent = ({children}) => {
     return (
         <div>
             <HeaderComponent/>
             {children}
-            <FooterComponent />
         </div>
     ) 
 }
