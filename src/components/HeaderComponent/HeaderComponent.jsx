a241cf1ee90d22f66d8a632fae54a0ce653c0147
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-20 11:28:59 +0700

    Position fixed for header component

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -6,14 +6,20 @@ import UndertakeComponent from '../UndertakeComponent/UndertakeComponent';
 
 const HeaderComponent = () => {
     return (
-        <div>
+        <div style={{
+            width: '100%',
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            zIndex: '1000'
+        }}>
             <WrapperHeader>
                 <Col span={6}>
                     <WrapperTextHeader>
                         PENGUINBOOKCORNER
                     </WrapperTextHeader>
                 </Col>
-                <Col span={12}>    
+                <Col span={12}>
                     <WrapperSearchHeader>
                         <SearchWrapper placeholder="Input search text" enterButton />
                     </WrapperSearchHeader>
