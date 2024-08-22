41a5d9b10d1dfa243abcc235db02ff3d43dd5d2c
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-22 13:54:34 +0700

    Navigation

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
 import { StyleMenu } from './style'
+import { useNavigate } from 'react-router-dom';
 
 const NavbarComponent = (props) => {
-    const { items } = props
+    const { items } = props;
+    const navigate = useNavigate();
+
+    const handleChangePage = (path) => {
+        navigate(`/${path}`, { replace: true });
+    };
+
+    const menuItems = items.map(item => ({
+        key: item.key,
+        label: item.icon ? (
+            <span>{item.icon} {item.label}</span>
+        ) : item.label,
+        onClick: () => item.path && handleChangePage(item.path)
+    }));
+
     return (
         <StyleMenu
             mode="inline"
-            items={items}
+            items={menuItems}
         />
     );
 };
 
-export default NavbarComponent
+export default NavbarComponent;
--- src/components/NavbarComponent/style.js
+++ src/components/NavbarComponent/style.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
 import { Menu } from 'antd'
 
+
 export const StyleMenu = styled(Menu)`
     width: 100%;
     border: none !important;
 
-    li.ant-menu-item-group > div {
-        padding: 0 !important;
+    li > span > span {
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-right: 4px;
+        }
     }
 `
