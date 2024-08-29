28c496a5d89e5854ea5a9d79aa45c27c1c53138d
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-29 17:32:46 +0700

    Fix bug: when click on button now can navigate

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -9,11 +9,11 @@ const NotFoundPage = () => {
         <NotFoundImg src='https://salt.tikicdn.com/ts/brickv2og/3c/7c/9b/1d101c4757843340d812828590283374.png' alt='Not Found 404'></NotFoundImg>
       </ImgWrapper>
       <NotFoundText>Trang bạn đang tìm kiếm không tồn tại</NotFoundText>
-      <NotFoundBtn type="primary">
-        <Link to='/'>
+      <Link to='/'>
+        <NotFoundBtn type="primary">
           Tiếp Tục Mua Sắm
-        </Link>
-      </NotFoundBtn>
+        </NotFoundBtn>
+      </Link>
     </NotFoundPageWrapper>
   )
 }
