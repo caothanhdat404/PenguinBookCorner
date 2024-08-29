2f8db1a0f0ae50448fd56aaa00568153945b3a54
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-29 17:33:17 +0700

    add signup service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
 export const loginUser = async(data) => {
-    const res = await axios.post(`${process.env.API_URL}/user/sign-in`)
+    const res = await axios.post(`http://localhost:3001/api/user/sign-in`, data)
+    return res.data
+}
+
+export const signupUser = async(data) => {
+    const res = await axios.post(`http://localhost:3001/api/user/sign-up`, data)
     return res.data
 }
