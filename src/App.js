79bf88fc4293adb32111a71c1889ba6c196a8a89
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-30 23:25:26 +0700

    Re-issue access token upon expiration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { jwtDecode } from "jwt-decode"
+import { useDispatch } from 'react-redux'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+import * as UserService from './services/UserService'
+import { updateUser } from './redux/user/userSlice'
+
+import { isJsonString } from "./utils"
 
 function App() {
+  useEffect(() => {
+      const { storageData, decoded } = handleDecoded()
+      if (decoded?.id) {
+        handleGetDetailsUser(decoded?.id, storageData)
+      }
+  }, [])
+
+
+  UserService.axiosJWT.interceptors.request.use(async (config) => {
+    const currentTime = new Date()
+    const { decoded } = handleDecoded()
+
+    if(decoded?.exp < currentTime.getTime() / 1000) {
+      const data = await UserService.refreshToken()
+      config.headers['token'] = `Bearer ${data?.access_token}`
+    }
+    return config;
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+  const dispatch = useDispatch()
+
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserService.getDetailsUser(id, token)
+    dispatch(updateUser({...res?.data, access_token: token}))
+  }
+
+  const handleDecoded = () => {
+    let storageData = localStorage.getItem('access_token')
+    let decoded = {}
+    if (storageData && isJsonString(storageData)) {
+      storageData = JSON.parse(storageData)
+      decoded = jwtDecode(storageData)
+    }
+    return { decoded, storageData }
+  }
+
   return (
     <div>
       <Router>
