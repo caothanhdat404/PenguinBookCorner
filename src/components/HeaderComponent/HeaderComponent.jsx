3541067be9beb78320cc1f50911e6ad45c0d2a7a
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-30 23:23:06 +0700

    Logout & Rendering after Login

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Col } from 'antd'
 import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
-import { WrapperHeader, WrapperTextHeader, WrapperSearchHeader, SearchWrapper, WrapperHeaderAccount, HeaderButton } from './style'
+import { WrapperHeader, WrapperTextHeader, WrapperSearchHeader, SearchWrapper, WrapperHeaderAccount, HeaderButton, StyleTippy, WrapperUserOption, UserOption } from './style'
 import UndertakeComponent from '../UndertakeComponent/UndertakeComponent';
+import Loading from '../../components/LoadingComponent/Loading'
+import * as UserService from '../../services/UserService'
+import { resetUser } from '../../redux/user/userSlice'
 
 const HeaderComponent = () => {
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(false)
+
+    const handleLogout = async () => {
+        setLoading(true)
+        await UserService.logoutUser()
+        dispatch(resetUser())
+        setLoading(false)
+    }
+
     return (
         <div style={{
             width: '100%',
@@ -29,12 +44,39 @@ const HeaderComponent = () => {
                 </Col>
                 <Col span={6}>
                     <WrapperHeaderAccount>
-                        <HeaderButton type="text" block>
-                            <Link to='/sign-in'>
-                                <UserOutlined />
-                                <span>Tài khoản</span>
-                            </Link>
-                        </HeaderButton>
+                        <Loading isLoading={loading}>
+                            {user?.name ?
+                                <StyleTippy
+                                    content={
+                                        <WrapperUserOption>
+                                            <UserOption>Thông tin người dùng</UserOption>
+                                            <UserOption onClick={handleLogout}>Đăng xuất</UserOption>
+                                        </WrapperUserOption>
+                                    }
+                                    interactive={true}
+                                    placement="bottom-end"
+                                >
+                                    <HeaderButton type="text" block>
+                                        <UserOutlined />
+                                        <div style={{
+                                            whiteSpace: 'nowrap',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis'
+                                        }}>
+
+                                            {user.name}
+                                        </div>
+                                    </HeaderButton>
+                                </StyleTippy>
+
+                                :
+                                <Link to='sign-in'>
+                                    <HeaderButton type="text" block>
+                                        <UserOutlined />
+                                        <div>Tài khoản</div>
+                                    </HeaderButton>
+                                </Link>}
+                        </Loading>
                         <HeaderButton type="text" block>
                             <ShoppingCartOutlined />
                             <span>Giỏ hàng</span>
