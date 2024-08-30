66ec19b6efa739939c29303a8b7db1c98ab6c5db
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-30 23:23:46 +0700

    Styling Tippy (use for interface after login)

--- src/components/HeaderComponent/style.js
+++ src/components/HeaderComponent/style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { Row, Input, Button } from "antd";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 const { Search } = Input;
 
@@ -25,6 +27,47 @@ export const SearchWrapper = styled(Search)`
   flex: 1;
 `;
 
+export const StyleTippy = styled(Tippy)`
+  width: 200px;
+  background-color: white;
+  color: black;
+  border: 1px;
+  box-shadow: rgba(0, 0, 0, 0.18) 0px 6px 12px 0px;
+  padding: 10px 0;
+
+  .tippy-box {
+    background-color: white;
+  }
+
+  .tippy-content {
+    padding: 0;
+  }
+
+  .tippy-arrow {
+    display: none;
+  }
+`;
+
+export const WrapperUserOption = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserOption = styled.div`
+  height: 38px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 8px 16px;
+  font-size: 14px;
+  text-align: left;
+
+  &:hover {
+    background-color: #cbcbcc;
+    cursor: pointer;
+  }
+`;
+
 export const WrapperHeaderAccount = styled.div`
   display: flex;
   justify-content: right;
@@ -34,4 +77,5 @@ export const HeaderButton = styled(Button)`
   max-width: 120px;
   color: #fff;
   font-size: 14px;
-`;
+
+`
