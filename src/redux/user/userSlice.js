b9bf435e9ba8ccd95523d62f06877dccb3fa7e3d
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-30 23:19:52 +0700

    Create redux for user

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    name: '',
+    email: '',
+    access_token: '',
+}
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateUser: (state, action) => {
+        const { name, email, access_token } = action.payload
+        state.name = name || email
+        state.email = email
+        state.access_token = access_token
+    },
+    resetUser: (state) => {
+      state.name = ''
+      state.email = ''
+      state.access_token = ''
+    }
+  },
+})
+
+export const { updateUser, resetUser } = userSlice.actions
+
+export default userSlice.reducer
