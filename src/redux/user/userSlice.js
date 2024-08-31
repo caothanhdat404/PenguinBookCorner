e245c1b803e63a349ab31b9c36365be2709a199e
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-31 16:53:13 +0700

    more data needed

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -3,21 +3,33 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     name: '',
     email: '',
-    access_token: '',
+    phone: '',
+    address: '',
+    avatar: '',
+    id: '',
+    access_token: ''
 }
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     updateUser: (state, action) => {
-        const { name, email, access_token } = action.payload
-        state.name = name || email
+        const { name = '', email, phone = '', address = '', avatar = '', access_token, _id } = action.payload
+        state.name = name
         state.email = email
+        state.phone = phone
+        state.address = address
+        state.avatar = avatar
+        state.id = _id
         state.access_token = access_token
     },
     resetUser: (state) => {
       state.name = ''
       state.email = ''
+      state.phone = ''
+      state.address = ''
+      state.avatar = ''
+      state.id = ''
       state.access_token = ''
     }
   },
