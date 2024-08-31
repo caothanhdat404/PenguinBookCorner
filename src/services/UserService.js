449a68c273e0433eca200b8027dfa9eae7029a64
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-31 16:53:52 +0700

    updateUser service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -32,3 +32,12 @@ export const refreshToken = async() => {
     })
     return res.data
 }
+
+export const updateUser = async(id, data, access_token ) => {
+    const res = await axiosJWT.put(`http://localhost:3000/api/user/update-user/${id}`, data, {
+        headers: {
+            token: `Bearer ${access_token}`
+        }
+    })
+    return res.data
+}
