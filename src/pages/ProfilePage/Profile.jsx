ec36a5c876d59d6262115f22c96fd0e16b620a3d
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-08-31 16:52:51 +0700

    create Profile User Page

---
+++ src/pages/ProfilePage/Profile.jsx
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from 'react'
+import { WrapperProfile, WrapperHeaderProfile, Container, WrapperInput, WrapperLabel } from './style'
+import InputForm from '../../components/InputForm/InputForm'
+import ButtonComponent from '../../components/ButtonComponent/ButtonComponent'
+import Loading from '../../components/LoadingComponent/Loading'
+import * as message from '../../components/Message/Message'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUser } from '../../redux/user/userSlice'
+
+import * as UserService from '../../services/UserService'
+
+import { useMutationHook } from '../../hooks/useMutationHook'
+
+const Profile = () => {
+  const user = useSelector((state) => state.user)
+  const [email, setEmail] = useState(user?.email)
+  const [name, setName] = useState(user?.name)
+  const [phone, setPhone] = useState(user?.phone)
+  const [address, setAddress] = useState(user?.address)
+  const [avatar, setAvatar] = useState(user?.avatar)
+
+  const dispatch = useDispatch()
+
+  const mutation = useMutationHook(
+    (data) => {
+      const { id, access_token, ...rests } = data
+      UserService.updateUser(id, rests, access_token)
+    }
+  )
+  const {isPending, isSuccess, isError} = mutation
+
+  useEffect(() => {
+    setEmail(user?.email)
+    setName(user?.name)
+    setPhone(user?.phone)
+    setAddress(user?.address)
+    setAvatar(user?.avatar)
+  }, [user])
+
+  useEffect(() => {
+    if (isSuccess) {
+      message.success()
+      handleGetDetailsUser(user?.id, user?.access_token)
+    } else if (isError) {
+      message.error()
+    }
+  }, [isSuccess, isError])
+
+
+  const handleOnchangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleOnchangeName = (e) => {
+    setName(e.target.value)
+  }
+  const handleOnchangePhone = (e) => {
+    setPhone(e.target.value)
+  }
+
+  const handleOnchangeAddress = (e) => {
+    setAddress(e.target.value)
+  }
+
+  const handleOnchangeAvatar = (e) => {
+    setAvatar(e.target.value)
+  }
+
+  const handleGetDetailsUser = async (id, token) => {
+    const res = await UserService.getDetailsUser(id, token)
+    dispatch(updateUser({...res?.data, access_token: token}))
+  }
+
+  const handleUpdate = () => {
+    mutation.mutate({ id: user?.id, email, name, phone, address, avatar, access_token: user?.access_token })
+  }
+
+
+  return (
+    <WrapperProfile>
+      <WrapperHeaderProfile>
+        Thông tin tài khoản
+      </WrapperHeaderProfile>
+      <Loading isLoading={isPending}>
+        <Container>
+          <WrapperInput>
+            <WrapperLabel htmlFor="email">Email:</WrapperLabel>
+            <InputForm style={{ width: '500px' }} id="email" value={email} onChange={handleOnchangeEmail} type=""/>
+            <ButtonComponent
+              onClick={handleUpdate}
+              size={40}
+              styleButton={{
+                background: 'transparent',
+                height: '30px',
+                width: '80px',
+                border: 'solid 1px rgb(26, 148, 255)',
+                borderRadius: '4px',
+                textAlign: 'center'
+              }}
+              textButton={'Thay đổi'}
+              styleTextButton={{
+                color: 'rgb(26, 148, 255)',
+                fontSize: '14px',
+                fontWeight: '500',
+              }}
+            ></ButtonComponent>
+          </WrapperInput>
+          <WrapperInput>
+            <WrapperLabel htmlFor="name">Họ và Tên:</WrapperLabel>
+            <InputForm style={{ width: '500px' }} id="name" value={name} onChange={handleOnchangeName}/>
+            <ButtonComponent
+              onClick={handleUpdate}
+              size={40}
+              styleButton={{
+                background: 'transparent',
+                height: '30px',
+                width: '80px',
+                border: 'solid 1px rgb(26, 148, 255)',
+                borderRadius: '4px',
+                textAlign: 'center'
+              }}
+              textButton={'Thay đổi'}
+              styleTextButton={{
+                color: 'rgb(26, 148, 255)',
+                fontSize: '14px',
+                fontWeight: '500',
+              }}
+            ></ButtonComponent>
+          </WrapperInput>
+          <WrapperInput>
+            <WrapperLabel htmlFor="phone">Số điện thoại:</WrapperLabel>
+            <InputForm style={{ width: '500px' }} id="phone" value={phone} onChange={handleOnchangePhone}/>
+            <ButtonComponent
+              onClick={handleUpdate}
+              size={40}
+              styleButton={{
+                background: 'transparent',
+                height: '30px',
+                width: '80px',
+                border: 'solid 1px rgb(26, 148, 255)',
+                borderRadius: '4px',
+                textAlign: 'center'
+              }}
+              textButton={'Thay đổi'}
+              styleTextButton={{
+                color: 'rgb(26, 148, 255)',
+                fontSize: '14px',
+                fontWeight: '500',
+              }}
+            ></ButtonComponent>
+          </WrapperInput>
+          <WrapperInput>
+            <WrapperLabel htmlFor="address">Địa chỉ:</WrapperLabel>
+            <InputForm style={{ width: '500px' }} id="address" value={address} onChange={handleOnchangeAddress}/>
+            <ButtonComponent
+              onClick={handleUpdate}
+              size={40}
+              styleButton={{
+                background: 'transparent',
+                height: '30px',
+                width: '80px',
+                border: 'solid 1px rgb(26, 148, 255)',
+                borderRadius: '4px',
+                textAlign: 'center'
+              }}
+              textButton={'Thay đổi'}
+              styleTextButton={{
+                color: 'rgb(26, 148, 255)',
+                fontSize: '14px',
+                fontWeight: '500',
+              }}
+            ></ButtonComponent>
+          </WrapperInput>
+          <WrapperInput>
+            <WrapperLabel htmlFor="avatar">Avatar:</WrapperLabel>
+            <InputForm style={{ width: '500px' }} id="avatar" value={avatar} onChange={handleOnchangeAvatar}/>
+            <ButtonComponent
+              onClick={handleUpdate}
+              size={40}
+              styleButton={{
+                background: 'transparent',
+                height: '30px',
+                width: '80px',
+                border: 'solid 1px rgb(26, 148, 255)',
+                borderRadius: '4px',
+                textAlign: 'center'
+              }}
+              textButton={'Thay đổi'}
+              styleTextButton={{
+                color: 'rgb(26, 148, 255)',
+                fontSize: '14px',
+                fontWeight: '500',
+              }}
+            ></ButtonComponent>
+          </WrapperInput>
+        </Container>
+      </Loading>
+    </WrapperProfile>
+  )
+}
+
+export default Profile
---
+++ src/pages/ProfilePage/style.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const WrapperProfile = styled.div`
+    padding: 116px 240px 20px;
+    background-color: #efefef;
+    height: 600px;
+`
+
+export const WrapperHeaderProfile = styled.div`
+    width: 100%;
+    height: 35px;
+    padding: 5px 0;
+    box-sizing: border-box;
+    color: #333;
+    font-size: 16px;
+    font-weight: 600;
+`
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    height: 550px;
+    background-color: #fff;
+    padding: 16px;
+    border-radius: 4px;
+`
+
+export const WrapperInput = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    width: 700px;
+    margin: 0 auto;
+`
+
+export const WrapperLabel = styled.label`
+    color: #000;
+    font-size: 12px;
+    line-height: 30px;
+    font-weight: 600;
+    min-width: 70px;
+`
