2737fa42da5379deff056689764de49a329458bf
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-01 22:56:00 +0700

    API create Product

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -4,3 +4,8 @@ export const getAllProduct = async(data) => {
     const res = await axios.get(`http://localhost:3000/api/product/getAll`)
     return res.data
 }
+
+export const createProduct = async(data) => {
+    const res = await axios.post('http://localhost:3000/api/product/create', data)
+    return res.data
+}
