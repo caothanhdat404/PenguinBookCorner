e2147657d2bf35000ca3c39e3989305b5aca67d3
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-01 22:54:40 +0700

    Construct Admin Page

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Col } from 'antd'
+import { UserOutlined, MailOutlined } from '@ant-design/icons'
+import HeaderComponent from '../../components/HeaderComponent/HeaderComponent'
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent'
+import AdminUser from '../../components/AdminUser/AdminUser'
+import AdminProduct from '../../components/AdminProduct/AdminProduct'
+import { WrapperAdminPage } from './style'
+
+const items = [
+  {
+    key: 'user',
+    label: 'Người dùng',
+    icon: <UserOutlined />
+  },
+  {
+    key: 'product',
+    label: 'Sản phẩm',
+    icon: <MailOutlined />
+  }
+]
 
 const AdminPage = () => {
+
+  const [keySelected, setKeySelected] = useState('');
+
+  const renderPage = (key) => {
+    switch (key) {
+      case 'user':
+        return (
+          <AdminUser />
+        )
+      case 'product':
+        return (
+          <AdminProduct />
+        )
+      default:
+        return <div>
+          Quản lý hệ thống
+        </div>
+    }
+  }
+
+  const handleDataFromChild = (childData) => {
+    setKeySelected(childData)
+  }
+
   return (
-    <div>AdminPage</div>
+    <>
+      <HeaderComponent isHiddenSearch isHiddenCart isHiddenUndertake />
+      <WrapperAdminPage>
+        <Col span={4}>
+          <div style={{ height: '100%' }}>
+            <div style={{ borderRight: 'solid 1px #ccc', height: '100%' }}>
+              <NavbarComponent items={items} sendData={handleDataFromChild} />
+            </div>
+          </div>
+        </Col>
+        <Col span={20}>
+          <div style={{ padding: '15px'}}>
+            {renderPage(keySelected)}
+          </div>
+        </Col>
+      </WrapperAdminPage>
+    </>
   )
 }
 
---
+++ src/pages/AdminPage/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Row } from 'antd'
+
+export const WrapperAdminPage = styled(Row)`
+    padding: 60px 0 0;
+    height: 728px;
+`
