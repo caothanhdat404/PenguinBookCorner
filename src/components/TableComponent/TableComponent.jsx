1449e0e9532b98c3bf038e3aa7e44e0f47eb24eb
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-03 23:42:52 +0700

    get data through props

--- src/components/TableComponent/TableComponent.jsx
+++ src/components/TableComponent/TableComponent.jsx
@@ -1,51 +1,10 @@
 import { Table } from 'antd'
 import React from 'react'
+import Loading from '../LoadingComponent/Loading';
 
 const TableComponent = (props) => {
-  const { selectionType = 'checkbox' } = props
+  const { selectionType = 'checkbox', data = [], isLoading = false, columns = [] } = props
 
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-    },
-  ];
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-    },
-    {
-      key: '4',
-      name: 'Disabled User',
-      age: 99,
-      address: 'Sydney No. 1 Lake Park',
-    },
-  ];
-  
   const rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
       console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
@@ -57,16 +16,17 @@ const TableComponent = (props) => {
   };
 
   return (
-    <div>
-      <Table 
+    <Loading isLoading={isLoading}>
+      <Table
         rowSelection={{
           type: selectionType,
           ...rowSelection
         }}
         columns={columns}
         dataSource={data}
+        {...props}
       />
-    </div>
+    </Loading>
   )
 }
 
