0f864e0e62b671c2cafc28620c841b26db6bcf45
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-04 23:54:45 +0700

    API update product

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { axiosJWT } from './UserService'
 
 export const getAllProduct = async(data) => {
     const res = await axios.get(`http://localhost:3000/api/product/getAll`)
@@ -12,5 +13,23 @@ export const createProduct = async(data) => {
 
 export const getDetailProduct = async(id) => {
     const res = await axios.get(`http://localhost:3000/api/product/get-details/${id}`)
+    return res.data
+}
+
+export const updateProduct = async(id, data, access_token) => {
+    const res = await axiosJWT.put(`http://localhost:3000/api/product/update/${id}`, data, {
+        headers: {
+            token: `Bearer ${access_token}`
+        }
+    })
+    return res.data
+}
+
+export const deleteProduct = async(id, access_token) => {
+    const res = await axiosJWT.delete(`http://localhost:3000/api/product/delete/${id}`, {
+        headers: {
+            token: `Bearer ${access_token}`
+        }
+    })
     return res.data
 }
