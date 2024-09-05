18ed3d0f11cedfefb876c0546c4bee495e0ae02c
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-05 23:34:01 +0700

    add product redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
 import UserReducer from './user/userSlice'
+import ProductReducer from './product/productSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    user: UserReducer
+    user: UserReducer,
+    product: ProductReducer
   },
 })
