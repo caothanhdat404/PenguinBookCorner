4ee37deb3be4afa189953468141639ff9bb3ddcf
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-07 00:02:04 +0700

    Search Product

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 import { axiosJWT } from './UserService'
 
-export const getAllProduct = async(limit) => {
-    const res = await axios.get(`http://localhost:3000/api/product/getAll?limit=${limit}`)
+export const getAllProduct = async(search, limit) => {
+    let res = []
+    if(search.length > 0) {
+        res = await axios.get(`http://localhost:3000/api/product/getAll?filter=name&filter=${search}?limit=${limit}`)
+    } else {
+        res = await axios.get(`http://localhost:3000/api/product/getAll?limit=${limit}`)
+    }
     return res.data
 }
 
