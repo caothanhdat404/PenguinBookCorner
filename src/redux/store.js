a492084cf694e0f1f01765ba50967af5b84c27c1
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-07 00:01:20 +0700

    create order redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
 import UserReducer from './user/userSlice'
 import ProductReducer from './product/productSlice'
+import OrderReducer from './order/orderSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     user: UserReducer,
-    product: ProductReducer
+    product: ProductReducer,
+    order: OrderReducer
   },
 })
