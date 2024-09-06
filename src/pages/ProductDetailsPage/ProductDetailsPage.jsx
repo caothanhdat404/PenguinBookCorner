3d4cd66071e01603e58ccdd8f9fcc5e665b777dc
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-06 23:59:50 +0700

    Add Breadcrumb

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import ProductDetailsComponent from '../../components/ProductDetailsComponent/ProductDetailsComponent'
+import { Link, useLocation, useParams } from 'react-router-dom'
+import { Breadcrumb } from 'antd'
+import { HomeOutlined } from '@ant-design/icons';
+
+import { WrapperBreadcrumd } from '../ProductPage/style'
 
 const ProductDetailsPage = () => {
+  const { id } = useParams()
+  const location = useLocation();
+  const data = location.state
+
+  const itemRender = (currentRoute, items, paths) => {
+    const isLast = currentRoute?.path === items[items.length - 1]?.path;
+
+    return isLast ? (
+      <span>{currentRoute.title}</span>
+    ) : (
+      <Link to={`/${paths.join("/")}`}>{currentRoute.title}</Link>
+    );
+  }
   return (
     <div style={{ padding: '116px 120px 10px', backgroundColor: '#efefef', height: '650px' }}>
-      <div>Trang chủ</div>
-      <ProductDetailsComponent />
+      <WrapperBreadcrumd>
+        <Breadcrumb
+          separator=">"
+          itemRender={itemRender}
+          items={[
+            {
+              path: '/',
+              title: <HomeOutlined />,
+            },
+            {
+              title: 'Chi tiết sản phẩm',
+            },
+            {
+              title: data
+            }
+          ]}
+        />
+      </WrapperBreadcrumd>
+
+      <ProductDetailsComponent idProduct={id} />
     </div>
   )
 }
