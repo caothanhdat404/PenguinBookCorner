724526a7b78034ee70d5dc195702f9f070385405
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-07 23:43:55 +0700

    New utils: convertPrice & renderOption

--- src/utils.js
+++ src/utils.js
@@ -14,3 +14,32 @@ new Promise((resolve, reject) => {
     reader.onload = () => resolve(reader.result);
     reader.onerror = (error) => reject(error)
 })
+
+export const renderOptions = (arr) => {
+    let result = []
+    if (arr) {
+        result = arr?.map((opt) => {
+            return {
+                value: opt,
+                label: opt
+            }
+        })
+    }
+    result.push({
+        label: 'Thêm type',
+        value: 'add_type'
+    })
+    return result
+}
+
+export const convertPrice = (price) => {
+    try {
+        const result = Number(price * 1000).toLocaleString('vi-VN', {
+            style: 'currency',
+            currency: 'VND',
+          })
+        return result
+    } catch (error) {
+        return null
+    }
+}
