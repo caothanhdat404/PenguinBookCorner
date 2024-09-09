43c05696afb71bb2da4e789401cb0a980c46a299
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-10 00:01:24 +0700

    Create Order Success Page

---
+++ src/pages/OrderSuccess/OrderSuccess.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { WrapperContainer, WrapperInfo, Label, WrapperValue, WrapperOrderItemInfo, WrapperItemOrder } from './style'
+import { useSelector } from 'react-redux'
+import { convertPrice } from '../../utils'
+import { useLocation } from 'react-router-dom'
+import { orderConstant } from '../../constant'
+
+const OrderSuccess = () => {
+  const order = useSelector((state) => state.order)
+  const location = useLocation()
+  const { state } = location
+
+  return (
+    <div style={{ background: "#f5f5fa", width: '100%', height: '100vh' }}>
+      <div style={{ height: '100%', width: '1270px', margin: '0 auto' }}>
+        <h3>Đơn hàng đã đặt thành công</h3>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <WrapperContainer>
+            <WrapperInfo>
+              <div>
+                <Label>Phương thức giao hàng</Label>
+                <WrapperValue>
+                  <span style={{ color: '#ea8500', fontWeight: 'bold' }}>{orderConstant.delivery[state?.delivery]}</span> Giao hàng tiết kiệm
+                </WrapperValue>
+              </div>
+            </WrapperInfo>
+            <WrapperInfo>
+              <div>
+                <Label>Phương thức thanh toán</Label>
+                <WrapperValue>
+                  {orderConstant.payment[state?.payment]}
+                </WrapperValue>
+              </div>
+            </WrapperInfo>
+            <WrapperOrderItemInfo>
+              {state.orders?.map((order) => {
+                return (
+                  <WrapperItemOrder>
+                    <div style={{ width: '500px', display: 'flex', alignItems: 'center', gap: '4px' }}>
+                      <img src={order.image} style={{ width: '77px', height: '79px', objectFit: 'cover' }} />
+                      <div style={{ width: '260px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{order?.name}</div>
+                    </div>
+                    <div style={{ flex: 1, display: 'flex', alignItems: 'center', gap: '10px' }}>
+                      <span>
+                        <span style={{ fontSize: '13px', color: '#242424' }}>Giá tiền: {convertPrice(order?.price)}</span>
+                      </span>
+                      <span>
+                        <span style={{ fontSize: '13px', color: '#242424' }}>Số lượng: {order?.amount}</span>
+                      </span>
+                    </div>
+                  </WrapperItemOrder>
+                )
+              })}
+            </WrapperOrderItemInfo>
+            <div>
+              <span style={{ fontSize: '16px', color: 'red' }}>Tổng tiền: {convertPrice(order?.totalPrice)}</span>
+            </div>
+          </WrapperContainer>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OrderSuccess
---
+++ src/pages/OrderSuccess/style.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const WrapperContainer = styled.div`
+    width: 100%;
+
+`
+
+export const WrapperInfo = styled.div`
+    padding: 17px 20px;
+    border-bottom: 1px solid #f5f5f5;
+    background: #fff;
+    border-top-right-radius: 6px;
+    border-top-left-radius: 6px;
+    width: 100%;
+`
+
+export const Label = styled.span`
+    font-size: 12px;
+    color: #000;
+    font-weight: bold
+`
+
+export const WrapperValue = styled.div`
+    background: rgb(240, 248, 255);
+    border: 1px solid rgb(194, 248, 255);
+    padding: 10px;
+    width: fit-content;
+    border-radius: 4px;
+    margin-top: 4px;
+`
+
+export const WrapperOrderItemInfo = styled.div`
+    padding: 17px 20px;
+    border-bottom: 1px solid #f5f5f5;
+    background: #fff;
+    border-top-right-radius: 6px;
+    border-top-left-radius: 6px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+
+export const WrapperItemOrder = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 9px 16px;
+    background: #fff;
+    margin-top: 12px;
+    justify-content: center;
+`
