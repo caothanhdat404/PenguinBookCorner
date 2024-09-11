3c93b194ba1b754a75204b54c9ba13e5bb40ac0c
caothanhdat404 <22520205@gm.uit.edu.vn>
2024-09-11 23:38:42 +0700

    get order by id service

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -8,3 +8,12 @@ export const createOrder = async(data, access_token) => {
     })
     return res.data
 }
+
+export const getOrderByUserId = async(id, access_token) => {
+    const res = await axios.get(`http://localhost:3000/api/order/get-order-details/${id}`, {
+        headers: {
+            token: `Bearer ${access_token}`
+        }
+    })
+    return res.data
+}
